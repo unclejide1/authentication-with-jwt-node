dc9ca98a663cc31acace611ddbff8f945e0f9e38
Jideofor <njusticej@gmail.com>
2020-05-04 17:45:02 +0100

    feat: set user registration with database

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+//routes
 const authRoute = require('./routes/auth')
 
+dotenv.config();
 
+//connect to db
+mongoose.connect(process.env.DB_CONNECT,
+{ useNewUrlParser: true, useUnifiedTopology: true  },
+ () => console.log('connected to db')
+)
+
+//Middleware
+app.use(express.json())
+//route middlewares
 app.use('/api/users', authRoute);
 
 
