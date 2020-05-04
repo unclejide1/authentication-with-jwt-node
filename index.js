b82d8da936bbb8a7afede79c8d3c6595328c5f7e
Jideofor <njusticej@gmail.com>
2020-05-04 16:51:34 +0100

    feat: connect to auth routes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const authRoute = require('./routes/auth')
 
+
+app.use('/api/users', authRoute);
 
 
 app.listen(3000, () => console.log('Up and Running'))
