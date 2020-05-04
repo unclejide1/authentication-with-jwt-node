3c15c6e059009374bbe4daf86c83b97f3abd38f9
Jideofor <njusticej@gmail.com>
2020-05-04 16:51:11 +0100

    feat: setup authentication routes

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+
+router.post('/register', (req, res) => {
+    res.send('Register');
+})
+
+router.post('/login', (req, res) => {
+    
+})
+
+module.exports = router
