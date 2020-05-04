2e93dc9e74c6244b47c2542960841f9fecdcd64c
Jideofor <njusticej@gmail.com>
2020-05-04 23:16:06 +0100

    feat: handle already existing users

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,12 @@ router.post('/register', async(req, res) => {
     
     const {error} = registerValidation(req.body)
     if(error) return res.status(400).send(error.details[0].message)
+
+    const emailExist = await User.findOne({email: req.body.email})
+
+    if(emailExist) return res.status(400).send('Email already Exists')
+
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
