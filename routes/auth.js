54d5ae31f4b358f55bb99babc26835b7994c35c5
Jideofor <njusticej@gmail.com>
2020-05-04 18:47:46 +0100

    feat: add schema validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const User = require('../model/User');
 
+//validation
+const {registerValidation}  = require('../validation');
 
 router.post('/register', async(req, res) => {
+    
+    const {error} = registerValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
     const user = new User({
         name: req.body.name,
         email: req.body.email,
