2f1b6af9d6f3cad315b4302976e106ed86af3216
Jideofor <njusticej@gmail.com>
2020-05-05 06:59:50 +0100

    feat: add log in feat

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const User = require('../model/User');
 const bcrypt = require('bcryptjs')
 
 //validation
-const {registerValidation}  = require('../validation');
+const {registerValidation, loginValidation}  = require('../validation');
 
 router.post('/register', async(req, res) => {
     
@@ -32,8 +32,20 @@ router.post('/register', async(req, res) => {
     }
 })
 
-router.post('/login', (req, res) => {
+router.post('/login', async(req, res) => {
+    const {error} = loginValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
 
+    const user = await User.findOne({email: req.body.email})
+
+    if(!user) return res.status(400).send('Email or Password is wrong')
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+
+    if(!validPassword) return res.status(400).send('Email or Password is wrong');
+
+
+    res.send('Logged in')
 })
 
 module.exports = router
